347667f13718ae261efbf0a22dd8e43dd99432d5
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-12-18 01:53:56 -0300

    Initial commit

---
+++ src/main/java/ipss/cl/collectorsallbum/CollectorsallbumApplication.java
@@ -0,0 +1,13 @@
+package ipss.cl.collectorsallbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectorsallbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectorsallbumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ipss/cl/collectorsallbum/CollectorsallbumApplicationTests.java
@@ -0,0 +1,13 @@
+package ipss.cl.collectorsallbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectorsallbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
