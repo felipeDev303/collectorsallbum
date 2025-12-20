c8d732e3b4defc7bf175f70fbfb301393fa620a2
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-12-19 21:42:46 -0300

    feat: actualización del README con mejoras en la descripción del proyecto y estado de sprints

--- src/main/java/ipss/cl/collectorsallbum/model/Lamina.java
+++ src/main/java/ipss/cl/collectorsallbum/model/Lamina.java
@@ -1,46 +1,27 @@
 package ipss.cl.collectorsallbum.model;
 
-import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 @Entity
-@EntityListeners(AuditingEntityListener.class) 
 public class Lamina {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotBlank(message = "El nombre es obligatorio")
+    
     private String nombre;
-
-    @Min(value = 1, message = "El número debe ser mayor a 0")
     private Integer numero;
-
-    private String tipo;
+    private String tipo; 
     private String imagenUrl; 
-
-    @NotNull
-    private Integer cantidad = 0;
-
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime fechaCreacion; 
+    
+    private boolean esRepetida;
+    private Integer cantidad; 
 
     @ManyToOne
     @JoinColumn(name = "album_id")
